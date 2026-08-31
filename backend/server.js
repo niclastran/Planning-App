88efb1ef41ec4b465defa558e1c7267b8b797839
Niclas Tran <niclastran987@gmail.com>
2026-08-31 23:21:01 +0200

    Add employees GET/POST routes, tested with Postman

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const PORT = 3001;
+let employees = [
+  { id: 1, name: "Anna", role: "Cashier" },
+  { id: 2, name: "Mikkel", role: "Manager" }
+];
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.post("/employees", (req, res) => {
+  const newEmployee = {
+    id: employees.length + 1,
+    name: req.body.name,
+    role: req.body.role
+  };
+  employees.push(newEmployee);
+  res.json(newEmployee);
 });
 
 app.listen(PORT, () => {
